71d9f93219239adc0097fecdee10638ec07cff1b
siftainejaz <siftainejaz@gmail.com>
2025-08-31 19:24:01 +0530

    implemented customReact functionality

--- React-Learnings-main/src/App.jsx
+++ React-Learnings-main/src/App.jsx
@@ -1,11 +1,12 @@
 import Test from './test.jsx'
 
 function App() {
+  const user = 'Siftain Ejaz';
 
   return (
     <>
       <Test/>
-      <h1>Test Module</h1>
+      <h1>Test Module written by {user} </h1>
     </>
   )
 }
--- React-Learnings-main/src/main.jsx
+++ React-Learnings-main/src/main.jsx
@@ -1,9 +1,41 @@
-import { StrictMode } from 'react'
+import React, { Children, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return (
+        <>
+            <h1>Custom MyApp !</h1>
+        </>
+    )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href : "http://google.com",
+//         target : "_link"
+//     },
+//     children : "Click to open Google"
+// }
+
+const user = "Siftain Ejaz";
+
+const reactElement = React.createElement(
+    'a',
+    {
+        href : 'http://google.com',
+        target : '_blank'
+    },
+    'Click to Open Google ',
+    user
+)
+
+const anotherElement = (
+    <a href='http://google.com' target='_Blank'>Open Google</a>
+)
+
 createRoot(document.getElementById('root')).render(
   
-    <App />
-  
+    reactElement
 )
