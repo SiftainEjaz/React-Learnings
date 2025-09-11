6f6cede41b6eca462dbef3a8ff920df3e6a16de5
siftainejaz <siftainejaz@gmail.com>
2025-09-11 13:30:17 +0530

    Revised props and tailwind

--- tailwindProps/src/App.jsx
+++ tailwindProps/src/App.jsx
@@ -3,13 +3,20 @@ import './App.css'
 import Card from './components/Card.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-  
+
+  let obj = {
+    name : "Saif",
+    age : 27
+  }
+
+  let arr = [1,3,5,6];
+
   return (
     <>
-      <h1 className='bg-green-300 text-black p-4 rounded-xl mb'>Tailwind Test</h1>
-      <Card username="Siftain Ejaz"  />
-      <Card username="Ejaz Ahmed" designation = "Railway Contractor" />
+      <h1 className='bg-green-300 text-black p-4 rounded-xl mb-6'>Tailwind Testing</h1>
+      <Card username="Siftain Ejaz" designation = "Senior Systems Engineer"/>
+      <Card username="Ejaz Ahmed" designation = "Railway Contractor"/> 
+      <Card />
     </>
   )
 }
