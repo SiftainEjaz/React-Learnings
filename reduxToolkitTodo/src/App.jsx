9ed0858c9c53bc3272e6ab97cfabfbd1d39c1c51
siftainejaz <siftainejaz@gmail.com>
2025-10-04 01:10:53 +0530

    Implemented a Todo using Redux Toolkit.

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import AddTodo from './components/addTodo'
+import Todo from './components/Todo'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Redux Toolkit</h1>
+      <AddTodo/>
+      <Todo/>
+    </>
+  )
+}
+
+export default App
